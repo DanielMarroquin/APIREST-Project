c1c2d193c93cb3c8f7337869edc1a4e98d9372f5
Daniel <dev.danielmarroquin25@gmail.com>
2021-08-02 10:22:19 -0500

    Inicio de proyecto

---
+++ APIRest-Project/Data/APIRest_ProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APIRest_Project.Models;
+
+namespace APIRest_Project.Data
+{
+    public class APIRest_ProjectContext : DbContext
+    {
+        public APIRest_ProjectContext (DbContextOptions<APIRest_ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APIRest_Project.Models.Persona> Persona { get; set; }
+    }
+}
---
+++ APIRest-Project/Models/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest_Project.Models
+{
+    public class Persona
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string habilidades { get; set; }
+
+
+    }
+}
